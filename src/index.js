65a8eafe8d642ed9e3a2c561823bbe2ab60456bb
priyanka yadav <py631588@gmail.com>
2021-01-11 10:11:49 +0530

    componentWillUnmount

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
+//import area
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Create an Child class Component
+class Child extends React.Component{
+    //1.property
+    
+    //2.Constructor
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    //3.Method
+    componentWillUnmount(){
+        alert('This child Component is going to unmount');
+    }
+    //Every method must have render method
+    render(){
+        //Every render method always return HTML
+        return  <h1>Welcome to Myclass</h1>;
+    }
+}
+
+//Create an Parent class Component
+class Myclass extends React.Component{
+    //1. Property
+  //  state = {};
+    //2. Constructor
+    constructor(props){
+        super(props);
+        //Th role of constructor is to initialize the members
+        this.state = {show:true};
+             
+    }
+
+    //3. Method
+    //Every method must have render method
+    deleteChild = ()=>{
+        //alert('hi');
+        this.setState({
+            show:false
+        });
+        //alert(this.state.show);
+    }
+    render(){
+        var header;//statement1
+        if(this.state.show){//statement2
+            //alert('True');
+            //console.log('true');
+            header = <Child/>;
+        }
+
+        //Every render method always return HTML
+        return(
+            <div>
+               { header }
+               <button onClick={ this.deleteChild}>Delete Child</button>
+            </div>
+        );
+    }
+}
+
+//object.method();
+ReactDOM.render(<Myclass />,document.getElementById('root'));//Actual argument 
